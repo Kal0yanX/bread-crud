344ee64a6a5d862d35def58ce69f6e6659357790
Juan Mireles <119257668+Kal0yanX@users.noreply.github.com>
2023-05-02 19:40:34 -0400

    added CSS and servers

--- controllers/bread.js
+++ controllers/bread.js
@@ -1,3 +1,4 @@
+const express = require('express')
 const router = require('express').Router()
 const Bread = require('../models/bread')
 
@@ -5,14 +6,16 @@ const Bread = require('../models/bread')
 router.get('/', (req, res) => {
 res.render('index', {
     breads: Bread
-})
-
+    })
 })
 
 //GET bread by
 router.get('/:index', (req, res) => {
     const { index } = req.params
-    res.send(Bread[index])
+    res.render('show', {
+        bread: Bread[index]
+    })
+    // res.send(Bread[index])
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const breadController =require('./controllers/bread')
 
 const app = express()
 
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+
 //middleware
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
